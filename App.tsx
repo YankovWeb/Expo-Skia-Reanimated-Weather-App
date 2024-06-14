8dfb48493e65ca4700d39c45615066caad27a643
YankovWeb <yankovWeb@outlook.com>
2024-06-15 01:22:53 +0300

    first

--- App.tsx
+++ App.tsx
@@ -1,41 +1,15 @@
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Canvas, LinearGradient, Rect, vec } from '@shopify/react-native-skia'
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
-import { useCallback } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Home from "./screens/Home";
-import BasicAnimations from "./screens/BasicAnimations";
-import RootNavigator from "./navigators/RootNavigator";
-import { NavigationContainer } from "@react-navigation/native";
-import { WeatherDataProvider } from "./context/WeatherDataContext";
 
-SplashScreen.preventAutoHideAsync();
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "SF-Thin": require("./assets/fonts/SF-Pro-Display-Thin.otf"),
-    "SF-Regular": require("./assets/fonts/SF-Pro-Display-Regular.otf"),
-    "SF-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
-  });
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-  if (!fontsLoaded) return null;
-  return (
-    <SafeAreaProvider onLayout={onLayoutRootView}>
-      <WeatherDataProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
 
-          <StatusBar style="light" />
-        </GestureHandlerRootView>
-      </WeatherDataProvider>
-    </SafeAreaProvider>
+  return (
+    <Canvas style={{ flex: 1 }}>
+      <Rect x={0} y={0} width={200} height={200}>
+
+        <LinearGradient start={vec(0, 0)} end={vec(200, 200)} colors={['red', 'green']} />
+      </Rect>
+    </Canvas>
   );
 }
--- data/CurrentWeather.ts
+++
@@ -1,9 +0,0 @@
-import { Weather } from "../models/Weather";
-
-export const currentWeather:Weather={
-    city:'Montreal',
-    temperature:19,
-    condition:'Mostly Clear',
-    high:24,
-    low:18
-}
